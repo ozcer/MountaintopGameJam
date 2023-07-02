0e4299659a3ffd72d9cac74203741480a44636fd
Kybre <hermanchan10@gmail.com>
2023-07-02 04:14:01 -0700

    Added Functionality for Opening Cutscene

---
+++ Assets/Scripts/SceneLoad.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string sceneName;
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
